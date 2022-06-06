0325aee6d5d2f1648fd4e211ed26ae6697a2b938
VietTinh <0306191473@caothang.edu.vn>
2022-06-06 15:25:19 +0700

    edit_template

--- resources/views/admin/src/product.blade.php
+++ resources/views/admin/src/product.blade.php
@@ -36,27 +36,7 @@
             </li>
         </ul>
     </header>
-    <!-- Sidebar menu-->
-    <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
-    <aside class="app-sidebar">
-        <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="/images/hay.jpg" width="50px" alt="User Image">
-            <div>
-                <p class="app-sidebar__user-name"><b>Cute Shop</b></p>
-                <p class="app-sidebar__user-designation">Chào mừng bạn trở lại</p>
-            </div>
-        </div>
-        <hr>
-        <ul class="app-menu">
-        <li><a class="app-menu__item" href="{{route('index')}}"><i class='app-menu__icon bx bx-home'></i><span class="app-menu__label">Trang Chủ</span></a></li>
-      <li><a class="app-menu__item" href="{{route('invoice')}}"><i class='app-menu__icon bx bx-task'></i><span class="app-menu__label"> Đơn Hàng</span></a></li>
-      <li><a class="app-menu__item" href="{{route('product')}}"><i class='app-menu__icon bx bx-purchase-tag-alt'></i><span class="app-menu__label">Sản Phẩm</span></a></li>
-      <li><a class="app-menu__item" href="{{route('staff')}}"><i class='app-menu__icon bx bx bx-run'></i><span class="app-menu__label">Quản lí nội bộ </span></a></li>
-      <li><a class="app-menu__item" href="{{route('money')}}"><i class='app-menu__icon bx bx-dollar'></i><span class="app-menu__label">Bảng Kê Lương</span></a></li>
-      <li><a class="app-menu__item" href="{{route('provided')}}"><i class='app-menu__icon bx bx-cart-alt'></i><span class="app-menu__label">Nhà cung cấp</span></a></li>
-      <li><a class="app-menu__item" href="{{route('report')}}"><i class='app-menu__icon bx bx-pie-chart-alt-2'></i><span class="app-menu__label">Báo cáo doanh thu</span></a></li>
-      <li><a class="app-menu__item" href="{{asset('')}}"><i class='app-menu__icon bx bx-cog'></i><span class="app-menu__label">Về Trang Khách Hàng</span></a></li>
-        </ul>
-    </aside>
+    @include('admin.menu_header')
     <main class="app-content">
         <div class="app-title">
             <ul class="app-breadcrumb breadcrumb side">
@@ -102,10 +82,12 @@
                                     <th>Mã sản phẩm</th>
                                     <th>Tên sản phẩm</th>
                                     <th>Ảnh</th>
-                                    <th>Số lượng</th>
-                                    <th>Tình trạng</th>
+                                    <th>Số lượng</th>                
                                     <th>Giá tiền</th>
+                                    <th>Thuế</th>
+                                    <th>Đã bán</th>
                                     <th>Danh mục</th>
+                                    <th>Tình trạng</th>
                                     <th>Chức năng</th>
                                 </tr>
                             </thead>
@@ -116,9 +98,11 @@
                                     <td>Giường ngủ Kara 1.6x2m</td>
                                     <td><img src="/img-sanpham/kara.jpg" alt="" width="100px;"></td>
                                     <td>60</td>
-                                    <td><span class="badge bg-success">Còn hàng</span></td>
                                     <td>14.500.000 đ</td>
+                                    <td>10%</td>
+                                    <td>20</td>
                                     <td>Giường người lớn</td>
+                                    <td><span class="badge bg-success">Còn hàng</span></td>                                         
                                     <td>
                                         <a href="#" class="btn btn-danger" style="font-size:7px;"><i class="fas fa-trash"></i></a>
                                         <a href="#" class="btn btn-warning" style="font-size:7px;"><i class="fas fa-edit"></i></a>
@@ -127,16 +111,19 @@
                                 <tr>
                                     <td width="10"><input type="checkbox" name="check1" value="1"></td>
                                     <td>73216006</td>
-                                    <td>Gối ngủ Kara 1.6x2m</td>
+                                    <td>Niệm ngủ Kara 1.6x2m</td>
                                     <td><img src="/img-sanpham/kara.jpg" alt="" width="100px;"></td>
                                     <td>60</td>
-                                    <td><span class="badge bg-success">Còn hàng</span></td>
                                     <td>14.500.000 đ</td>
-                                    <td>Gối người lớn</td>
+                                    <td>10%</td>
+                                    <td>20</td>
+                                    <td>Giường người lớn</td>
+                                    <td><span class="badge bg-success">Còn hàng</span></td>                                         
                                     <td>
                                         <a href="#" class="btn btn-danger" style="font-size:7px;"><i class="fas fa-trash"></i></a>
                                         <a href="#" class="btn btn-warning" style="font-size:7px;"><i class="fas fa-edit"></i></a>
                                     </td>
+                                </tr>
                                 </tr>
                             </tbody>
                         </table>
